cb44c2589d53061d7140bc5f966725fde19f0b83
jum0 <wnsah052@naver.com>
2020-10-04 23:56:17 +0900

    dp / check

---
+++ Boj/dp/1699.py
@@ -0,0 +1,13 @@
+import math
+
+N = int(input())
+dp = [0] * (N+1)
+
+for i in range(1, N+1):
+    dp[i] = i
+
+    for j in range(1, int(math.sqrt(i)+1)):
+        if dp[i-j*j] + 1 < dp[i]:
+            dp[i] = dp[i-j*j] + 1
+
+print(dp[N])
